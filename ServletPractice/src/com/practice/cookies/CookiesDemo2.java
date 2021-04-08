7f582d088f1b5076dff5bddcfbf27cec4d9cbebf
jahidmomin <mominjahid313@gmail.com>
2021-04-08 14:40:54 +0530

    used cookies

--- ServletPractice/src/com/practice/cookies/CookiesDemo2.java
+++ ServletPractice/src/com/practice/cookies/CookiesDemo2.java
@@ -19,34 +19,37 @@ public class CookiesDemo2 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();
 		
 //		fetch cookies values.
 		Cookie [] carr=request.getCookies();
 		boolean f=false;
+		String name=null;
 		
 		if (carr==null) {
-			response.sendRedirect("/request1");
+			response.sendRedirect("cookieForm.html");
 		} else {
 			for (Cookie cookie : carr) {
 				String tname=cookie.getName();
 				if (tname.equals("username")) {
 					f=true;
-				}else {
-					
+					name=cookie.getValue();
 				}
-				
 			}
 		}
 		
-		out.print("<h1>Hello ,"+request.getParameter("name")+"</h1>\r\n" + 
-				"	<p>Welcome Back To Website</p>\r\n" + 
-				"	");
-		out.print("<a href='CookiesDemo3'>Click To GO Next Page</a>");
 		
-		
-		
+		if (f) {
+			out.print("<h1>Hello ,"+name+"</h1>\r\n" + 
+					"<p>Welcome Back To Website</p>\r\n" + 
+					" ");
+			out.print("<a href='request3'>Click To GO Next Page</a>");
+		} else {
+			out.print("<h1>New User Detect</h1>");
+			out.print("<a href='request3'>Click To GO Next Page</a>");
+		}
+	
 		
 	}
 
--- ServletPractice/src/com/practice/cookies/CookiesDemo3.java
+++ ServletPractice/src/com/practice/cookies/CookiesDemo3.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +13,41 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class CookiesDemo3
  */
-@WebServlet("/CookiesDemo3")
+@WebServlet("/request3")
 public class CookiesDemo3 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();
-		out.print("<h1>Hello ,"+request.getParameter("name")+"</h1>\r\n" + 
-				"	<p>Welcome Back Again To Website</p>\r\n" + 
-				"	");
-		out.print("Thank You .. Visit Again");
+		
+		//cookie check
+		
+		Cookie [] carr=request.getCookies();
+		boolean f=false;
+		String name=null;
+		
+		if (carr==null) {
+			response.sendRedirect("request2");
+		} else {
+			for (Cookie cookie : carr) {
+				String tname=cookie.getName();
+				if (tname.equals("username")) {
+					f=true;
+					name=cookie.getValue();
+				}
+			}
+		}
+		
+		
+		
+		if (f) {
+			out.print("<h1>Hello ,"+name+"</h1>\r\n" + 
+					"	<p>Welcome Back Again To Website</p>\r\n" + 
+					"	");
+			out.print("Thank You .. Visit Again");
+		} else {
+			out.print("You Are New User");
+		}
 	}
 
 }
